44e2406b8506f9fdbf0f2c30b5a689a26b380dcf
Samuel Lin <sabaoth1114@gmail.com>
2023-04-11 18:03:20 +0800

    [feat][currencies send function]

--- nextjs-basic/pages/index.tsx
+++ nextjs-basic/pages/index.tsx
@@ -1,124 +1,135 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { GetServerSideProps } from 'next';
+import { DatePicker, Input, Table } from 'antd';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import type { Dayjs } from 'dayjs';
+import dayjs from 'dayjs';
+const { Search } = Input;
+const { RangePicker } = DatePicker;
+type RangeValue = [Dayjs | null, Dayjs | null] | null;
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface IAccountProps {
+  txs: Array<ITxRow>;
 }
+
+interface ITxRow {
+  key: string;
+  symbol: string;
+  address: string;
+  amount: number;
+}
+
+interface IAccountDetailResponse {
+  data: IAccData
+}
+
+interface IAccData {
+  flow: IAccFlow
+}
+
+interface IAccFlow {
+  inputs: Array<IAccTx>;
+}
+
+interface IAccTx {
+  amount: number;
+  count: string;
+  currency: IAccCurrency;
+}
+
+interface IAccCurrency {
+  address: string;
+  symbol: string;
+}
+
+export default function Home({ txs }: IAccountProps) {
+  const router = useRouter();
+  const dateFormat = 'YYYY-MM-DD';
+
+  const [inputAddress, SetInputAddress] = useState('');
+  const [dates, setDates] = useState<RangeValue>(null);
+  const [value, setValue] = useState<RangeValue>(null);
+
+  const columns = [
+    {
+      title: 'Currency',
+      dataIndex: 'symbol',
+      key: 'symbol',
+    },
+    {
+      title: 'Smart Contract Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      key: 'amount',
+    },
+  ];
+
+  const onSearch = (addr: string) => {
+    const startDate = dayjs(dates?.[0]).format(dateFormat);
+    const endDate = dayjs(dates?.[1]).format(dateFormat);
+    router.push(`/?address=${addr}&startDate=${startDate}&endDate=${endDate}`);
+    SetInputAddress(addr);
+  }
+
+  return (
+    <div className='home-container'>
+      <Search placeholder="input search text" onSearch={onSearch} />
+      <RangePicker format={dateFormat} 
+        onCalendarChange={(val) => setDates(val)}
+        onChange={(val) => setValue(val)}/>
+      <br/>
+      <span>Currencies Send:</span><br/>
+      <Table dataSource={txs} columns={columns} />
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query: { address, startDate, endDate } }) => {
+  let txs = new Array<ITxRow>();
+  if (address) {
+    const res = await axios.post('https://graphql.bitquery.io/', {
+      "variables": {
+        "limit": 10,
+        "offset": 0,
+        "address": address,
+        "network": "flow",
+        "from": startDate + "T00:00:00",
+        "till": endDate + "T23:59:59",
+        "dateFormat": "%Y-%m-%d"
+      },
+      "query": "query ($network: FlowNetwork!, $limit: Int!, $offset: Int!, $from: ISO8601DateTime, $till: ISO8601DateTime, $address: String!) {\n  flow(network: $network) {\n    inputs(options: {limit: $limit, offset: $offset, desc: \"count\"}, date: {since: $from, till: $till}, address: {is: $address}) {\n      amount(calculate: sum, address: {is: $address})\n      count: countBigInt(address: {is: $address})\n      currency {\n        address\n        symbol\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"
+    }, {
+      headers: {
+        'content-type': 'application/json; charset=utf-8',
+        'X-API-KEY': 'BQYR8h7uD7S8klT2q6BZoa9m1duLyA95'
+      }
+    });
+
+    const post: IAccountDetailResponse = await res.data;
+    post.data.flow.inputs.map((record: IAccTx, index: number) => {
+      const tx: ITxRow = {
+        key: index.toString(),
+        symbol: record.currency.symbol,
+        address: record.currency.address,
+        amount: record.amount
+      };
+      txs.push(tx);
+    });
+    
+    const wrapper: IAccountProps = {
+      txs: txs
+    };
+  }
+
+  return {
+    props: {
+      txs,
+    },
+  };
+
+};
